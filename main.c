5d21d849c8243260444b2c2f1a928d32b8e1157c
me996 <me996@gmail.com>
2023-09-16 13:45:42 +0100

    simple_shell

--- main.c
+++ main.c
@@ -1,17 +1,16 @@
 #include "shell.h"
 /**
  * main - simple shell
- *@ac: argument count
- *@av: array of arguments (strings)
+ *@argc: argument count
+ *@argv: array of arguments (strings)
  *Return: 0 always success
  */
 int main(int argc, char **argv)
 {
     char *cmd = NULL;
-    /*char **cmd = NULL;*/
+    char **cmd = NULL;
     int status = 0;
     (void) argc;
-    (void) argv;
     write(STDOUT_FILENO, "$ ", 2);
     while (1)
     {
@@ -22,8 +21,8 @@ int main(int argc, char **argv)
            return (status);
         }
         free (cmd);
-        /*cmd = spliter(line);*/
-        /*status = _execve(cmd, argv);*/
+        cmd = spliter(line);
+        status = _execve(cmd, argv);
 
     }
 }
