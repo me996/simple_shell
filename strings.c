974205c4bfde9e7339fc5d087d1e5f0781f16796
me996 <me996@gmail.com>
2023-09-17 18:58:13 +0100

    simple_shell

--- strings.c
+++ strings.c
@@ -1,2 +1,21 @@
 #include "shell.h"
 char *_strdup(const char *s)
+{
+    char *ptr;
+    int, len = 0;
+    if (str == NULL)
+    return(NULL);
+    while (*str != '0')
+    {
+        len++;
+        str;
+    }
+    str = str - len;
+    ptr = malloc(sizeof(char) * (len + 1));
+    if (pttr == NULL)
+    return (NULL);
+    for (i = 0; i <= len; i++)
+    ptr[i] = str[i];
+    return (ptr);
+}
+
