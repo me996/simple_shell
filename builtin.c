cb08fc95c85668a35ee6f5342c3c7044cd01a6a7
me996 <me996@gmail.com>
2023-09-22 08:46:10 -0700

    simple shell

--- builtin.c
+++ builtin.c
@@ -12,18 +12,39 @@ return (0);
 }
 void handle_built_in(char **input_cmd, char **argv, int *stat, int cpt)
 {
-(void) argv;
-(void) cpt;
 if (_strcmp(input_cmd[0], "exit") == 0)
-exit_cmd(input_cmd, stat);
+exit_cmd(input_cmd, argv, stat, cpt);
 else if
 (_strcmp(input_cmd[0], "env") == 0)
 printenv(input_cmd, stat);
 }
-void exit_cmd (char **input_cmd, int *stat)
+void exit_cmd (char **input_cmd,char **argv, int *stat, int cpt)
 {
+int value = (*stat);
+char *idx, message[] = ": exit: Illegal number: ";
+if (input_cmd[1])
+{
+if (positive_num(input_cmd[1]))
+{
+value = _atoi(input_cmd[1]);
+}
+else
+{
+idx = _itoa(cpt);
+write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+write(STDERR_FILENO, ": ", 2);
+write(STDERR_FILENO, idx, _strlen(idx));
+write(STDERR_FILENO, message, _strlen(message));
+write(STDERR_FILENO, input_cmd[1], _strlen(input_cmd[1]));
+write(STDERR_FILENO, "\n", 1);
+free(idx);
 freeleak(input_cmd);
-exit(*stat);
+(*stat) = 2;
+return;
+}
+}
+freeleak(input_cmd);
+exit(value);
 }
 void printenv (char **input_cmd, int *stat)
 { 
@@ -36,4 +57,3 @@ write(STDOUT_FILENO, "\n", 1);
 freeleak(input_cmd);
 (*stat) = 0;
 }
-
--- shell.h
+++ shell.h
@@ -25,8 +25,10 @@ char *_itoa(int s);
 void reverse_string(char *s, int len);
 int built_in(char *input_cmd);
 void handle_built_in(char **input_cmd, char **argv, int *stat, int cpt);
-void printenv (char **input_cmd, int *stat);
-void exit_cmd (char **input_cmd, int *stat);
+void printenv (char ** input_cmd, int *stat);
+void exit_cmd (char **input_cmd,char **argv, int *stat, int cpt);
+int _atoi(char *s);
+int positive_num(char *s);
 
 int _strcmp(char *s1, char *s2);
 char *_strcat(char *dest, char *src);
